ff28df040a4d129afd9fccd35bf2c9becee8c30d
Ben Browning <bbrownin@redhat.com>
2010-09-01 14:10:22 -0400

    Enable IntenseDebate extension

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -4,5 +4,6 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Paginator.new(:posts, '/news/index', :per_page => 5)
   extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::Atomizer.new(:posts, '/news.atom')
+  extension Awestruct::Extensions::IntenseDebate.new()
 end
 
